1e69a6f726f9d5fa27a8ab242bc8bf2acdd18979
haymovich <bar.rose65@gmail.com>
2022-12-11 00:47:20 +0200

    Last Release Tag -> [v1.0.0] | finish logger

--- server/lib/gitManager.py
+++ server/lib/gitManager.py
@@ -84,7 +84,6 @@ class GitHubManager:
         try:
             extractReleaseTag = self.sendCommandToTerminalAndGetOutpout(
                 'git describe')
-            print('---------------------------------- ',extractReleaseTag)
             if 'False' in str(extractReleaseTag):
                 return 'v1.0.0'
             else:
@@ -176,12 +175,13 @@ class GitHubManager:
                 tagVersion = f'v{firstIndex}.{secondIndex}.{thirdIndex}'
             else:
                 tagVersion = 'v1.0.0'
-            # Create auto tag to git
-            subprocess.call(
-                ["git", "tag", "-a", "-m", f"Release from adam framework - {setenceToReleaseTypeStr}", f"{tagVersion}"])
-            subprocess.call(["git", "push", "origin", f"--tags"])
-            self.pushBasicToGithub(
-                f'Release from tag method --> {setenceToReleaseTypeStr}')
+            print(tagVersion)
+            # # Create auto tag to git
+            # subprocess.call(
+            #     ["git", "tag", "-a", "-m", f"{setenceToReleaseTypeStr}", f"{tagVersion}"])
+            # subprocess.call(["git", "push", "origin", f"--tags"])
+            # self.pushBasicToGithub(
+            #     f'Release from tag method --> {setenceToReleaseTypeStr}')
 
 
 # Running only if system in this file
