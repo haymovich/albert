713e40767e5799a4268927533f7c04d01edd5303
haymovich <bar.rose65@gmail.com>
2022-12-12 00:43:48 +0200

    LRT : [v2.0.21] | update_setup.py_to_be_with_print_and_alias_inside

--- setup.py
+++ setup.py
@@ -10,12 +10,17 @@ pathScriptFolder = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(1, os.path.join(pathScriptFolder, 'lib'))
 from utils import Utils
 from aliasManager import AliasManager
+from logger import logger
 # ------- # Outside Variable  - albert searcher # ------- #
 scriptNickname = '-setup' 
-
+log = logger(False)
 
 if __name__ == "__main__":
-
+    Utils().spliter()
     Utils().installLib()
     Utils().checkLib()
+    Utils().spliter()
     AliasManager().injectAlbertAlias()
+    Utils().spliter()
+    log.printLog(1,'Finish install all albert req files / alias / etc - it\'s recomended to do bash after.')
+    
